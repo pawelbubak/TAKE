d6dd022f3794c01bad9485d96acc30ab75ef0627
pawelbubak <pawelbubak97@gmail.com>
2020-05-16 17:44:21 +0200

    TAKE 9

---
+++ CompanyApi/CompanyApi/Models/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CompanyApi.Models
+{
+    public partial class Department
+    {
+        public Department()
+        {
+            Employee = new HashSet<Employee>();
+        }
+
+        public int DepartmentId { get; set; }
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [InverseProperty("Department")]
+        public ICollection<Employee> Employee { get; set; }
+    }
+}
